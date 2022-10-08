9039871c019f89e3351bcde22fff6167f24c027b
guzma <guzma@DESKTOP-8I3HSO5>
2022-10-08 18:01:51 -0500

    Commit

---
+++ Lab6_tarea/Lab6_tarea/PageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6_tarea
+{
+    public class PageModel
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public PageModel(string title, string subtitle)
+        {
+            Title = title;
+            Subtitle = subtitle;
+        }
+    }
+}
